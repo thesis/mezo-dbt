d633a75ef85b0801309681738ef2898f16c8cfe9
benedikt-buchert <benedikt@mohrstade.de>
2025-07-24 16:58:35 +0200

    refactor: improve formatting and readability of SQL staging model for goldsky trove liquidated

--- models/10_staging/stg_goldsky_musd_trove_manager__trove_liquidated.sql
+++ models/10_staging/stg_goldsky_musd_trove_manager__trove_liquidated.sql
@@ -1,30 +1,39 @@
-with source as (
-        select * from
-        {{ source('raw_goldsky', 'raw_goldsky_musd_trove_manager__trove_liquidated') }}
-  ),
+with
+    source as (
+        select *
+        from
+            {{
+                source(
+                    "raw_goldsky", "raw_goldsky_musd_trove_manager__trove_liquidated"
+                )
+            }}
+    ),
 
-  renamed as (
-      select
-        vid,
-        --`block`, --duplicate of block_number
-        id,
-        block_number,
-        transaction_hash,
-        contract_id,
-        borrower,
-        debt,
-        coll as collateral,
-        operation,
-        _gs_chain,
-        _gs_gid,
-        timestamp_millis(cast(`timestamp` as int)) as record_timestamp
-      from source
-  ),
+    renamed as (
+        select
+            vid,
+            -- `block`, --duplicate of block_number
+            id,
+            block_number,
+            transaction_hash,
+            contract_id,
+            borrower,
+            debt,
+            coll as collateral,
+            operation,
+            _gs_chain,
+            _gs_gid,
+            timestamp_millis(cast(`timestamp` as int)) as record_timestamp
+        from source
+    ),
 
- transformed_fields as (
-      select * except(debt, collateral),
-        {{ format_musd_currency_columns(['debt', 'collateral']) }}
-      from renamed
- )
+    transformed_fields as (
+        select
+            * except (debt, collateral, operation),
+            {{ categorize_operation("operation") }} as operation,
+            {{ format_musd_currency_columns(["debt", "collateral"]) }}
+        from renamed
+    )
 
-  select * from  transformed_fields
+select *
+from transformed_fields

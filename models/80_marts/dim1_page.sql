296fece87a9f5343b5f5fb00738b83ee9ec6bc67
benedikt-buchert <benedikt@mohrstade.de>
2025-08-19 16:37:02 +0200

    fix: make pages dim unique

--- models/80_marts/dim1_page.sql
+++ models/80_marts/dim1_page.sql
@@ -9,7 +9,13 @@ with
         select landing_page_url_without_query_string as id
         from segment_sessions
         where landing_page_url_without_query_string is not null
+    ),
+
+    make_unique as (
+        select id
+        from all_pages
+        qualify row_number() over (partition by id order by id) = 1
     )
 
 select *
-from all_pages
+from make_unique

3c356bbe7e2ca70f0d6b8b0935b70529b662d40b
benedikt-buchert <benedikt@mohrstade.de>
2025-08-19 16:27:45 +0200

    fix: page dim

--- models/80_marts/dim1_page.sql
+++ models/80_marts/dim1_page.sql
@@ -4,10 +4,12 @@ with
     all_pages as (
         select exit_page_url_without_query_string as id
         from segment_sessions
+        where exit_page_url_without_query_string is not null
         union all
         select landing_page_url_without_query_string as id
         from segment_sessions
+        where landing_page_url_without_query_string is not null
     )
 
-select distinct id
+select *
 from all_pages

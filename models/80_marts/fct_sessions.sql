91502e3da6a6c9de3c1a827bbe971a54124dfe6f
benedikt-buchert <benedikt@mohrstade.de>
2025-08-20 16:43:23 +0200

    chore: add all metrics defined in config

--- models/80_marts/fct_sessions.sql
+++ models/80_marts/fct_sessions.sql
@@ -9,7 +9,9 @@ with
             campaign_id as fk_campaign,
             landing_page_url_without_query_string as fk_landingpage__dim1_page,
             exit_page_url_without_query_string as fk_exitpage__dim1_page,
-            page_views,
+            {% for session_metric in var("session_metrics") %}
+                {{ session_metric.metric_name }}{{ "," if not loop.last }}
+            {% endfor %},
             duration_in_s as session_duration_in_seconds
         from segment_sessions
     )

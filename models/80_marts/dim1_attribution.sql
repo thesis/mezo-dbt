76b25ce7d95479957543ea34c814f836eea2de9f
Diana Shalai <diana@mohrstade.de>
2025-09-24 10:24:21 +0200

    added dim table with ("referrer_source", "referrer_medium")

---
+++ models/80_marts/dim1_attribution.sql
@@ -0,0 +1,22 @@
+with
+    base as (
+
+        select distinct referrer_source, referrer_medium
+        from {{ ref("int_segment__sessions") }}
+
+    ),
+
+    with_keys as (
+
+        select
+            {{
+                dbt_utils.generate_surrogate_key(
+                    ["referrer_source", "referrer_medium"]
+                )
+            }} as channel_id, referrer_source, referrer_medium
+        from base
+
+    )
+
+select *
+from with_keys

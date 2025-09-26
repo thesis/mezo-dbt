5730e6b7247a334cd20f5cf3758ad14a0517bbf0
DianaMariia <diana@mohrstade.de>
2025-09-26 10:24:34 +0200

    added raw data from google sheet and created union paid data (#30)
    * added raw data from google sheet and created union paid data
    
    * fix pre-commit
    
    * fix pre commit
    
    * issue with relations
    
    * none

--- models/80_marts/fct_paid.sql
+++ models/80_marts/fct_paid.sql
@@ -2,7 +2,14 @@ with
     all_paid as (select * from {{ ref("int_paid__all_paid") }}),
 
     fact as (
-        select id, campaign_id as fk__dim1_campaign, spend, impressions, clicks
+        select
+            paid_id as id,
+            date_day as record_date,
+            spend,
+            impressions,
+            clicks,
+            referrer_source,
+            referrer_medium
         from all_paid
     )
 
